4e553f5dd92ac31d44c47f1d474cef0481330c3b
kertaskids <kertaskids@gmail.com>
2017-03-20 07:57:24 +0800

    Updating Space Setting Window

--- Editor/DungeonGenerator/views/Space/SettingsWindow.cs
+++ Editor/DungeonGenerator/views/Space/SettingsWindow.cs
@@ -6,78 +6,81 @@ using EditorAdvance = EditorExtend.Advance;
 using EditorStyle = EditorExtend.Style;
 
 namespace SpaceGrammar {
-
-	//Enums
-	public enum StartingSymbolType{
-		Symbol1,
-		Symbol2
-	}
-	public enum DefaultSymbolType{
-		DefaultSymbol1,
-		DefaultSymbol2
-	}
-	public enum ConnectingFloorType{
-		Floor,
-		Grass
-	}
-	public enum ConnectingWallType{
-		Wall,
-		Window, 
-		Door
-	}
-
 	public class SettingsWindow : EditorWindow {
+		//[Edit later] change the string type with SpaceGrammar...
+		// Types - Symbol & Connection
+		private int _startingSymbolIndex;
+		private int _defaultSymbolIndex;
+		private int _connectingFloorIndex;
+		private int _connectingWallIndex;
+		private string[] _symbolTypes;
+		private string[] _connectingFloorTypes;
+		private string[] _connectingWallTypes;
+		private string _startingSymbol;
+		private string _defaultSymbol;
+		private string _connectingFloor;
+		private string _connectingWall;
 
-		//Types
-		private StartingSymbolType _startingSymboltype;
-		private DefaultSymbolType _defaultSymboltype;
-		private ConnectingFloorType _connectingFloorType;
-		private ConnectingWallType _connectingWallType;
-
-		//Info
+		// Info
 		private string _messageInfo;
 
-		//[Remove soon] 
+		// Apply Button Enabled
 
 		void Awake(){
-			_startingSymboltype = StartingSymbolType.Symbol1;
-			_defaultSymboltype = DefaultSymbolType.DefaultSymbol1;
-			_connectingFloorType = ConnectingFloorType.Floor;
-			_connectingWallType = ConnectingWallType.Wall;
+			_startingSymbolIndex = 0;
+			_defaultSymbolIndex = 1;
+			_connectingFloorIndex = 0;
+			_connectingWallIndex = 0;
+
+			// [Edit later] Initialization 
+			_symbolTypes = new string[]{ "Entrance", "Task" };
+			_connectingFloorTypes = new string[]{ "Floor", "Basement" };
+			_connectingWallTypes = new string[]{ "Wall", "Window" };
+			_startingSymbol = _symbolTypes[_startingSymbolIndex];
+			_defaultSymbol = _symbolTypes[_defaultSymbolIndex];
+			_connectingFloor = _connectingFloorTypes[_connectingFloorIndex];
+			_connectingWall = _connectingWallTypes[_connectingWallIndex];
+
 			_messageInfo = "Info!\nNothing to show. ";
 		}
 
 		void OnGUI() {
 			//Symbol Block
-			GUILayout.Space (EditorStyle.PaddingAfterBlock);
-			GUILayout.Label ("Symbol");
-			_startingSymboltype = (StartingSymbolType)EditorGUILayout.EnumPopup ("Starting Symbol", _startingSymboltype);
-			_defaultSymboltype = (DefaultSymbolType)EditorGUILayout.EnumPopup ("Default Symbol", _defaultSymboltype);
+			GUILayout.Space(EditorStyle.PaddingAfterBlock);
+			GUILayout.Label("Symbol");
+			_startingSymbolIndex = EditorGUILayout.Popup("Starting Symbol", _startingSymbolIndex, _symbolTypes);
+			_defaultSymbolIndex = EditorGUILayout.Popup("Default Symbol", _defaultSymbolIndex, _symbolTypes);
 
 			//Connection Block
 			GUILayout.Space (EditorStyle.PaddingAfterBlock);
 			GUILayout.Label ("Connection");
-			_connectingFloorType = (ConnectingFloorType)EditorGUILayout.EnumPopup ("Connectiong Floor", _connectingFloorType);
-			_connectingWallType = (ConnectingWallType)EditorGUILayout.EnumPopup ("Connecting Wall", _connectingWallType);
+			_connectingFloorIndex = EditorGUILayout.Popup("Connecting Floor", _connectingFloorIndex, _connectingFloorTypes);
+			_connectingWallIndex = EditorGUILayout.Popup("Connecting Wall", _connectingWallIndex, _connectingWallTypes);
 
 			//Info Block
 			GUILayout.Space (EditorStyle.PaddingAfterBlock);
-			EditorGUILayout.HelpBox (_messageInfo,MessageType.Info);
-			if (GUILayout.Button ("Apply", EditorStyles.miniButtonMid, EditorStyle.ButtonHeight)) {
-				if (EditorUtility.DisplayDialog ("Saving the Grammar Settings",
-					   "Are you sure to save?",
-					   "Yes", "No")) {
+			EditorGUILayout.HelpBox(_messageInfo,MessageType.Info);
+			if (GUILayout.Button("Apply", EditorStyles.miniButtonMid, EditorStyle.ButtonHeight)) {
+				// [Edit later]
+				_startingSymbol = _symbolTypes[_startingSymbolIndex];
+				_defaultSymbol = _symbolTypes[_defaultSymbolIndex];
+				_connectingFloor = _connectingFloorTypes[_connectingFloorIndex];
+				_connectingWall = _connectingWallTypes[_connectingWallIndex];
+
+				if (EditorUtility.DisplayDialog("Saving the Grammar Settings",
+					   "Are you sure to save?", "Yes", "No")) {
+					// [Edit later] Just to make sure we render to strings
 					_messageInfo = "Info\n" + 
-						_startingSymboltype.ToString() + ", " + 
-						_defaultSymboltype.ToString() + ", " +
-						_connectingFloorType.ToString() + ", " +
-						_connectingWallType.ToString() + " have been saved. ";
+						_startingSymbol.ToString() + ", " + 
+						_defaultSymbol.ToString() + ", " +
+						_connectingFloor.ToString() + ", " +
+						_connectingWall.ToString() + " have been saved. ";
 				} else {
 					_messageInfo = "Info\n" + 
-						_startingSymboltype.ToString() + ", " + 
-						_defaultSymboltype.ToString() + ", " +
-						_connectingFloorType.ToString() + ", " +
-						_connectingWallType.ToString()  + " not yet saved! ";
+						_startingSymbol.ToString() + ", " + 
+						_defaultSymbol.ToString() + ", " +
+						_connectingFloor.ToString() + ", " +
+						_connectingWall.ToString() + " not yet saved. ";
 				}
 			}
 		}
