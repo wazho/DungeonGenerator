73f59877d5c6c2ffca9c4d99bebb9ec2f153ed45
kertaskids <kertaskids@gmail.com>
2017-04-14 14:44:49 +0800

    Uploading SampleStyle, SampleStyleWindow, Drawing

--- Editor/models/EditorExtend/SampleStyle.cs
+++ Editor/models/EditorExtend/SampleStyle.cs
@@ -7,6 +7,21 @@ using EditorStyle   = EditorExtend.Style;
 
 namespace EditorExtend {
 	public class SampleStyle{
+
+		public enum ButtonType {
+			Regular, 
+			Mid,
+			Left,
+			Right
+		};
+		public enum CustomButtonColor {
+			Blue,
+			Green,
+			Grey,
+			Yellow,
+			Orange
+		};
+
 		// Textures
 		public static Texture2D SplashImage { get { return Resources.Load<Texture2D>("Images/Splash"); } }
 
@@ -75,8 +90,53 @@ namespace EditorExtend {
 		public static Texture2D RightActiveOrange { get { return Resources.Load<Texture2D>("Images/UI/Orange/right_active_orange") as Texture2D; } }
 		public static Texture2D RightActiveYellow { get { return Resources.Load<Texture2D>("Images/UI/Yellow/right_active_yellow") as Texture2D; } }
 
+		// Colors
+		public static Color ColorDarkestBlue { get { return new Color(22f / 255f, 110f / 255f, 147f / 255f); } }
+		public static Color ColorDarkBlue { get { return new Color(29f / 255f, 167f / 255f, 226f/ 255f); } }
+		public static Color ColorBlue { get { return new Color(64f / 255f, 181f / 255f, 225f/ 255f); } }
+		public static Color ColorLightBlue { get { return new Color(118f / 255f, 208f / 255f, 255f / 255f); } }
+		public static Color ColorLightestBlue { get { return new Color(166f / 255f, 226f / 255f, 255f / 255f); } }
+		public static Color ColorDarkestGreen { get { return new Color(22f / 255f, 100f / 255f, 91f / 255f); } }
+		public static Color ColorDarkGreen { get { return new Color(79f / 255f, 167f / 255f, 121f/ 255f); } }
+		public static Color ColorGreen { get { return new Color(119f / 255f, 207f / 255f, 104f/ 255f); } }
+		public static Color ColorLightGreen { get { return new Color(136f / 255f, 224f / 255f, 96f / 255f); } }
+		public static Color ColorLightestGreen { get { return new Color(158f / 255f, 249f / 255f, 144f / 255f); } }
+		public static Color ColorDarkestGrey { get { return new Color(49f / 255f, 81f / 255f, 99f / 255f); } }
+		public static Color ColorDarkGrey { get { return new Color(103f / 255f, 153f / 255f, 184f/ 255f); } }
+		public static Color ColorGrey { get { return new Color(130f / 255f, 174f / 255f, 192f/ 255f); } }
+		public static Color ColorLightGrey { get { return new Color(206f / 255f, 219f / 255f, 225f / 255f); } }
+		public static Color ColorLightestGrey { get { return new Color(238f / 255f, 238f / 255f, 238f / 255f); } }
+		public static Color ColorDarkestOrange { get { return new Color(193f / 255f, 125f / 255f, 74f / 255f); } }
+		public static Color ColorDarkOrange { get { return new Color(216f / 255f, 126f / 255f, 65f/ 255f); } }
+		public static Color ColorOrange { get { return new Color(250f / 255f, 129f / 255f, 50f/ 255f); } }
+		public static Color ColorLightOrange { get { return new Color(250f / 255f, 159f / 255f, 71f / 255f); } }
+		public static Color ColorLightestOrange { get { return new Color(250f / 255f, 196f / 255f, 91f / 255f); } }
+		public static Color ColorDarkestYellow { get { return new Color(197f / 255f, 191f / 255f, 91f / 255f); } }
+		public static Color ColorDarkYellow { get { return new Color(220f / 255f, 201f / 255f, 83f/ 255f); } }
+		public static Color ColorYellow { get { return new Color(255f / 255f, 217f / 255f, 72f/ 255f); } }
+		public static Color ColorLightYellow { get { return new Color(255f / 255f, 223f / 255f, 143f / 255f); } }
+		public static Color ColorLightestYellow { get { return new Color(255f / 255f, 240f / 255f, 168f / 255f); } }
+
 		// Fonts
-		public static Font TabElementFont { get { return Resources.Load<Font>("Fonts/kenvector_future") as Font; } }
+		public static Font TabFont { get { return Resources.Load<Font>("Fonts/kenvector_future") as Font; } }
+
+		// Grid's default parameters
+		private const int _minorGridSize = 20;
+		public static int MinorGridSize {
+			get { return _minorGridSize; }
+		}
+		private const int _majorGridSize = 100;
+		public static int MajorGridSize {
+			get { return _majorGridSize; }
+		}
+		private static Color _gridColor = ColorLightBlue;
+		public static Color GridColor {
+			get { return _gridColor; }
+		}
+		private static Color _gridBackgroundColor = ColorLightestBlue;
+		public static Color GridBackgroundColor {
+			get { return _gridBackgroundColor; }
+		}
 
 		// Font size ; Generally same with the things in CommonStyle class. The default alignment is center. 
 		private const int _headerOneFontSize = 24;
@@ -96,6 +156,37 @@ namespace EditorExtend {
 			get { return _contentFontSize; }
 		}
 
+		// Layout heights ; Generally same with things in CommonStyle class. 
+		private const int _headerOneHeight = 30;
+		public static int HeaderOneHeight {
+			get { return _headerOneHeight; }
+		}
+		public static GUILayoutOption HeaderOneHeightLayout {
+			get { return GUILayout.Height(_headerOneHeight); }
+		}
+		private const int _headerTwoHeight = 24;
+		public static int HeaderTwoHeight {
+			get { return _headerTwoHeight; }
+		}
+		public static GUILayoutOption HeaderTwoHeightLayout {
+			get { return GUILayout.Height(_headerTwoHeight); }
+		}
+		private const int _headerThreeHeight = 20;
+		public static int HeaderThreeHeight {
+			get { return _headerThreeHeight; }
+		}
+		public static GUILayoutOption HeaderThreeHeightLayout {
+			get { return GUILayout.Height(_headerThreeHeight); }
+		}
+		private const int _contentHeight = 18;
+		public static int ContentHeight {
+			get { return _contentHeight; }
+		}
+		public static GUILayoutOption ContentHeightLayout {
+			get { return GUILayout.Height(_contentHeight); }
+		}
+
+
 		// Padding, Margin, Offset 
 		private const int _paddingArea = 10;
 		public static int PaddingArea {
@@ -105,7 +196,7 @@ namespace EditorExtend {
 		public static int PaddingBlock {
 			get { return _paddingBlock; }
 		}
-	
+
 		// Button heights ; Generally, same with in CommonStyle
 		private const int _buttonHeight = 20;
 		public static GUILayoutOption ButtonHeight {
@@ -124,6 +215,16 @@ namespace EditorExtend {
 			get { return GUILayout.Height(_mainButtonHeight); }
 		}
 
+		// Textfield and textarea heights
+		private const int _textFieldHeight = 15;
+		public static GUILayoutOption TextFieldHeight {
+			get { return GUILayout.Height(_textFieldHeight); }
+		}
+		private const int _textAreaHeight  = 60;
+		public static GUILayoutOption TextAreaHeight {
+			get { return GUILayout.Height(_textAreaHeight); }
+		}
+
 		// GUIStyles
 		// Header & Content styles ; Generally same with the things in CommonStyle class. The default alignment is center. 
 		private static GUIStyle _headerOne = new GUIStyle(EditorStyles.label);
@@ -131,6 +232,7 @@ namespace EditorExtend {
 			get {
 				_headerOne.fontSize  = 24;
 				_headerOne.alignment = TextAnchor.MiddleCenter;
+				_headerOne.font = TabFont;
 				return _headerOne;
 			}
 		}
@@ -167,9 +269,6 @@ namespace EditorExtend {
 				_button.normal.background = RegularNormalBlue;
 				_button.hover.background = RegularHoverBlue;
 				_button.active.background = RegularActiveBlue;
-				_button.onNormal.background = RegularNormalBlue;
-				_button.onHover.background = RegularHoverBlue;
-				_button.onActive.background = RegularActiveBlue;
 				_button.normal.textColor = Color.white;
 				_button.hover.textColor = Color.white;
 				_button.active.textColor = Color.white;
@@ -183,9 +282,6 @@ namespace EditorExtend {
 				_buttonLeft.normal.background = LeftNormalBlue;
 				_buttonLeft.hover.background = LeftHoverBlue;
 				_buttonLeft.active.background = LeftActiveBlue;
-				_buttonLeft.onNormal.background = LeftNormalBlue;
-				_buttonLeft.onHover.background = LeftHoverBlue;
-				_buttonLeft.onActive.background = LeftActiveBlue;
 				_buttonLeft.normal.textColor = Color.white;
 				_buttonLeft.hover.textColor = Color.white;
 				_buttonLeft.active.textColor = Color.white;
@@ -194,14 +290,11 @@ namespace EditorExtend {
 			set { }
 		}
 		private static GUIStyle _buttonMid = new GUIStyle(EditorStyles.miniButtonMid);
-		public static GUIStyle ButtonMid{
+		public static GUIStyle ButtonMid {
 			get { 
 				_buttonMid.normal.background = MidNormalBlue;
 				_buttonMid.hover.background = MidHoverBlue;
 				_buttonMid.active.background = MidActiveBlue;
-				_buttonMid.onNormal.background = MidNormalBlue;
-				_buttonMid.onHover.background = MidHoverBlue;
-				_buttonMid.onActive.background = MidActiveBlue;
 				_buttonMid.normal.textColor = Color.white;
 				_buttonMid.hover.textColor = Color.white;
 				_buttonMid.active.textColor = Color.white;
@@ -209,15 +302,12 @@ namespace EditorExtend {
 			}
 			set { }
 		}
-		private static GUIStyle _buttonRight= new GUIStyle(EditorStyles.miniButtonRight);
-		public static GUIStyle ButtonRight{
+		private static GUIStyle _buttonRight = new GUIStyle(EditorStyles.miniButtonRight);
+		public static GUIStyle ButtonRight {
 			get { 
 				_buttonRight.normal.background = RightNormalBlue;
 				_buttonRight.hover.background = RightHoverBlue;
 				_buttonRight.active.background = RightActiveBlue;
-				_buttonRight.onNormal.background = RightNormalBlue;
-				_buttonRight.onHover.background = RightHoverBlue;
-				_buttonRight.onActive.background = RightActiveBlue;
 				_buttonRight.normal.textColor = Color.white;
 				_buttonRight.hover.textColor = Color.white;
 				_buttonRight.active.textColor = Color.white;
@@ -226,10 +316,237 @@ namespace EditorExtend {
 			set { }
 		}
 
+		public static GUIStyle GetButtonStyle(ButtonType buttonType, CustomButtonColor customButtonColor){
+			GUIStyle buttonStyle = new GUIStyle();
+
+			if (buttonType == ButtonType.Regular) {
+				buttonStyle = new GUIStyle(Button);
+
+				switch (customButtonColor) {
+				case CustomButtonColor.Green:
+					buttonStyle.normal.background = RegularNormalGreen;
+					buttonStyle.hover.background = RegularHoverGreen;
+					buttonStyle.active.background = RegularActiveGreen;
+					break;
+				case CustomButtonColor.Grey:
+					buttonStyle.normal.background = RegularNormalGrey;
+					buttonStyle.hover.background = RegularHoverGrey;
+					buttonStyle.active.background = RegularActiveGrey;
+					break;
+				case CustomButtonColor.Orange:
+					buttonStyle.normal.background = RegularNormalOrange;
+					buttonStyle.hover.background = RegularHoverOrange;
+					buttonStyle.active.background = RegularActiveOrange;
+					break;
+				case CustomButtonColor.Yellow:
+					buttonStyle.normal.background = RegularNormalYellow;
+					buttonStyle.hover.background = RegularHoverYellow;
+					buttonStyle.active.background = RegularActiveYellow;
+					break;
+				case CustomButtonColor.Blue: 
+					buttonStyle = Button;	
+					break;
+				}
+			} else if (buttonType == ButtonType.Mid) {
+				buttonStyle = new GUIStyle(ButtonMid);
+				switch (customButtonColor) {
+				case CustomButtonColor.Green:
+					buttonStyle.normal.background = MidNormalGreen;
+					buttonStyle.hover.background = MidHoverGreen;
+					buttonStyle.active.background = MidActiveGreen;
+					break;
+				case CustomButtonColor.Grey:
+					buttonStyle.normal.background = MidNormalGrey;
+					buttonStyle.hover.background = MidHoverGrey;
+					buttonStyle.active.background = MidActiveGrey;
+					break;
+				case CustomButtonColor.Orange:
+					buttonStyle.normal.background = MidNormalOrange;
+					buttonStyle.hover.background = MidHoverOrange;
+					buttonStyle.active.background = MidActiveOrange;
+					break;
+				case CustomButtonColor.Yellow:
+					buttonStyle.normal.background = MidNormalYellow;
+					buttonStyle.hover.background = MidHoverYellow;
+					buttonStyle.active.background = MidActiveYellow;
+					break;
+				case CustomButtonColor.Blue: 
+					buttonStyle = ButtonMid;
+					break;
+				}
+			} else if (buttonType == ButtonType.Left) {
+				buttonStyle = new GUIStyle(ButtonLeft);
+				switch (customButtonColor) {
+				case CustomButtonColor.Green:
+					buttonStyle.normal.background = LeftNormalGreen;
+					buttonStyle.hover.background = LeftHoverGreen;
+					buttonStyle.active.background = LeftActiveGreen;
+					break;
+				case CustomButtonColor.Grey:
+					buttonStyle.normal.background = LeftNormalGrey;
+					buttonStyle.hover.background = LeftHoverGrey;
+					buttonStyle.active.background = LeftActiveGrey;
+					break;
+				case CustomButtonColor.Orange:
+					buttonStyle.normal.background = LeftNormalOrange;
+					buttonStyle.hover.background = LeftHoverOrange;
+					buttonStyle.active.background = LeftActiveOrange;
+					break;
+				case CustomButtonColor.Yellow:
+					buttonStyle.normal.background = LeftNormalYellow;
+					buttonStyle.hover.background = LeftHoverYellow;
+					buttonStyle.active.background = LeftActiveYellow;
+					break;
+				case CustomButtonColor.Blue: 
+					buttonStyle = ButtonLeft;
+					break;
+				}
+			} else if (buttonType == ButtonType.Right) {
+				buttonStyle = new GUIStyle(ButtonRight);
+				switch (customButtonColor) {
+				case CustomButtonColor.Green:
+					buttonStyle.normal.background = RightNormalGreen;
+					buttonStyle.hover.background = RightHoverGreen;
+					buttonStyle.active.background = RightActiveGreen;
+					break;
+				case CustomButtonColor.Grey:
+					buttonStyle.normal.background = RightNormalGrey;
+					buttonStyle.hover.background = RightHoverGrey;
+					buttonStyle.active.background = RightActiveGrey;
+					break;
+				case CustomButtonColor.Orange:
+					buttonStyle.normal.background = RightNormalOrange;
+					buttonStyle.hover.background = RightHoverOrange;
+					buttonStyle.active.background = RightActiveOrange;
+					break;
+				case CustomButtonColor.Yellow:
+					buttonStyle.normal.background = RightNormalYellow;
+					buttonStyle.hover.background = RightHoverYellow;
+					buttonStyle.active.background = RightActiveYellow;
+					break;
+				case CustomButtonColor.Blue: 
+					buttonStyle = ButtonRight;
+					break;
+				}
+			} 
+
+			return buttonStyle;
+		}
+
+		// Boxes
+		private static GUIStyle _box = new GUIStyle(GUI.skin.box);
+		public static GUIStyle Box {
+			get {
+				// Change the background later
+				_box.normal.background = GUI.skin.window.active.background;
+				return _box;
+			}
+		}
+
+		// Frames
+		private static GUIStyle _frameLightestBlue = new GUIStyle(GUI.skin.box);
+		public static GUIStyle FrameLightestBlue {
+			get { 
+				Texture2D t = new Texture2D(1, 1);
+				t.SetPixel(0, 0, ColorLightestBlue);
+				t.Apply();
+				_frameLightestBlue.normal.background = t; // GUI.skin.window.active.background;
+				return _frameLightestBlue;
+			}
+		}
+
+		public static GUIStyle Frame(Color color){
+			GUIStyle frame = new GUIStyle(GUI.skin.box);
+			frame.normal.background = GetTextureFromColor(color);
+			return frame;
+		}
+
+		// TextArea and Textfield styles, label etc
+		private static GUIStyle _textArea = new GUIStyle(EditorStyles.textArea);
+		public static GUIStyle TextArea {
+			get { 
+				_textArea.focused.background = GetTextureFromColor(ColorLightBlue);
+				_textArea.normal.textColor = Color.black;
+				_textArea.focused.textColor = Color.white;
+				return _textArea;
+			}
+			set { }
+		}
+		private static GUIStyle _textField = new GUIStyle(EditorStyles.textField);
+		public static GUIStyle TextField {
+			get { 
+				_textField.focused.background = GetTextureFromColor(ColorLightBlue);
+				_textField.normal.textColor = Color.black;
+				_textField.focused.textColor = Color.white;
+				return _textField;
+			}
+			set { }
+		}
+
+		private static GUIStyle _textFieldLabel = new GUIStyle(EditorStyles.label);
+		public static GUIStyle TextFieldLabel {
+			get { 
+				_textFieldLabel.focused.textColor = ColorDarkestBlue;
+				return _textFieldLabel;
+			}
+			set { }
+		}
+		private static GUIStyle _textAreaLabel = new GUIStyle(EditorStyles.label);
+		public static GUIStyle TextAreaLabel {
+			get {
+				_textAreaLabel.focused.textColor = ColorDarkestBlue;
+				return _textAreaLabel;
+			}
+			set { }
+		}
+
+		public static Texture2D GetTextureFromColor(Color color){
+			Texture2D texture = new Texture2D(1, 1);
+			texture.SetPixel(0, 0, color);
+			texture.Apply();
+			return texture;
+		}
+
 		public static void DebugRect(Rect rect, Color color){
 			color.a = 0.5f;
 			EditorGUI.DrawRect(rect, color); 
 			// Debug.Log("Rect "+color.ToString()+" = (X : " + rect.x + ", Y: " + rect.y + "). (W: " + rect.width + ", H: " + rect.height + "). (Cnt X: " + rect.center.x + ", Cnt Y: " + rect.center.y + ")");
+		}
+
+		public static Rect GetRectOfWindow(){
+			return new Rect(0, 0, Screen.width, Screen.height);
+		}
+
+		public static void DrawWindowBackground(Color color){
+			EditorGUI.DrawRect(GetRectOfWindow(), color);
+		}
+		// Suggestion : default color for background is light grey/ light blue, default color for grid is white or light blue.
+		public static void DrawGrid(Rect rect, int minor, int major, Color rectColor, Color gridColor){
+			//Color RectColor = rectColor ?? new Color(166f / 255f, 226f / 255f, 255f / 255f);
+			//Color GridColor = gridColor ?? new Color(118f / 255f, 208f / 255f, 255f / 255f);
+			// Draw Background
+			EditorGUI.DrawRect(rect, rectColor); 
+
+			// Vertical lines
+			int i,j;
+			Vector3[] majorLines; 
+			for(i = 0, j = 0; i<= rect.width; i += minor, j += major){
+				Handles.BeginGUI();
+				Handles.color = gridColor;
+				Handles.DrawLine(new Vector2(rect.x + i, rect.y), new Vector2(rect.x + i, rect.height));
+				majorLines = new [] {new Vector3(rect.x + j, rect.y), new Vector3(rect.x + j, rect.height)};
+				Handles.DrawAAPolyLine(3f, majorLines);
+				Handles.EndGUI();
+			}
+			// Horizontal lines
+			for(i = 0, j = 0; i<= rect.height; i += minor, j += major){
+				Handles.BeginGUI();
+				Handles.color = gridColor;
+				Handles.DrawLine(new Vector2(rect.x, rect.y + i), new Vector2(rect.width, rect.y + i));
+				majorLines = new [] {new Vector3(rect.x, rect.y + j), new Vector3(rect.width, rect.y + j)};
+				Handles.DrawAAPolyLine(3f, majorLines);
+				Handles.EndGUI();
+			}
 		}
 	}
 
@@ -246,6 +563,7 @@ namespace EditorExtend {
 		}
 		public static Rect IndexWindowButtonsCanvasArea {
 			get { 
+				_indexWindowSplashCanvasArea.x = 0;
 				_indexWindowButtonsCanvasArea.y = _indexWindowSplashCanvasArea.y + _indexWindowSplashCanvasArea.height;
 				_indexWindowButtonsCanvasArea.width = Screen.width;
 				_indexWindowButtonsCanvasArea.height = Screen.height - _indexWindowSplashCanvasArea.height;
@@ -253,5 +571,4 @@ namespace EditorExtend {
 			}
 		}	
 	}
-
 }
--- Editor/views/SampleStyleWindow.cs
+++ Editor/views/SampleStyleWindow.cs
@@ -6,6 +6,7 @@ using EditorAdvance = EditorExtend.Advance;
 using EditorStyle   = EditorExtend.CommonStyle;
 using SampleStyle = EditorExtend.SampleStyle;
 using Container = EditorExtend.IndexWindow;
+//using SampleDrawing = EditorExtend.Drawing;
 
 public class SampleStyleWindow : EditorWindow {
 	private bool _isInitButtonTexture;
@@ -13,6 +14,9 @@ public class SampleStyleWindow : EditorWindow {
 	public GUIStyle LeftButtonStyle;
 	public GUIStyle MidButtonStyle;
 	public GUIStyle RightButtonStyle;
+	public int toolbarInt = 0;
+	public string[] toolbarStrings = new string[] {"Toolbar1", "Toolbar2", "Toolbar3"};
+	Vector2 ScrollPosition;
 
 	public enum SampleTab {
 		Left,
@@ -22,16 +26,27 @@ public class SampleStyleWindow : EditorWindow {
 	private SampleTab _sampleTab;
 
 	void Awake(){
+		ScrollPosition = Vector2.zero;
 		_isInitButtonTexture = true;
 		_sampleTab = SampleTab.Left;
 		LeftButtonStyle = new GUIStyle(SampleStyle.ButtonLeft);
 		MidButtonStyle = new GUIStyle(SampleStyle.ButtonMid);
 		RightButtonStyle = new GUIStyle(SampleStyle.ButtonRight);
-		LeftButtonStyle.font = SampleStyle.TabElementFont;
-		RightButtonStyle.font = SampleStyle.TabElementFont;
+		LeftButtonStyle.font = SampleStyle.TabFont;
+		RightButtonStyle.font = SampleStyle.TabFont;
+		Debug.Log("Texture Size WH " + LeftButtonStyle.normal.background.width + "" + LeftButtonStyle.normal.background.height);
+		Debug.Log("LFS: " + LeftButtonStyle.lineHeight + "," + LeftButtonStyle.fixedHeight+ "," + LeftButtonStyle.stretchHeight);
+		Debug.Log("Border Size LRTB: " + LeftButtonStyle.border.left + "," + LeftButtonStyle.border.right + "," + LeftButtonStyle.border.top + "," + LeftButtonStyle.border.bottom);
+		Debug.Log("Padding Size LRTB: " + LeftButtonStyle.padding.left + "," + LeftButtonStyle.padding.right + "," + LeftButtonStyle.padding.top + "," + LeftButtonStyle.padding.bottom);
 	}
 
 	void OnGUI(){
+		SampleStyle.DrawWindowBackground(SampleStyle.ColorLightestGrey);
+		ScrollPosition = GUILayout.BeginScrollView(ScrollPosition, GUILayout.Height(500));
+		SampleStyle.DrawGrid(SampleStyle.GetRectOfWindow(), SampleStyle.MinorGridSize, SampleStyle.MajorGridSize, new Color(166f/255f, 226f/255f, 255f/255f), new Color(118f / 255f, 208f / 255f, 255f / 255f));
+
+		//toolbarInt = GUILayout.Toolbar(toolbarInt, toolbarStrings, SampleStyle.Button,SampleStyle.MainButtonHeight);
+
 		// Change this later
 		EditorGUILayout.BeginHorizontal();
 		if (GUILayout.Button("Left", LeftButtonStyle, GUILayout.Height(25))) {
@@ -49,7 +64,7 @@ public class SampleStyleWindow : EditorWindow {
 			LeftButtonStyle.normal.background = SampleStyle.LeftActiveBlue;
 			MidButtonStyle.normal.background = SampleStyle.MidNormalBlue;
 			RightButtonStyle.normal.background = SampleStyle.RightNormalBlue;
-			EditorStyle.HeaderOne.font = SampleStyle.TabElementFont;
+			EditorStyle.HeaderOne.font = SampleStyle.TabFont;
 			GUILayout.Label("Left", EditorStyle.HeaderOne);
 			break;
 		case SampleTab.Mid:
@@ -66,6 +81,75 @@ public class SampleStyleWindow : EditorWindow {
 			break;
 		}
 		GUILayout.Label("This is a content");
+		Rect r = new Rect(10, 10, 100, 100);
+		/*
+		EditorGUI.DrawRect(r, new Color(30f/255f,167f/255, 225f/255f));
+		GUILayout.BeginArea(r);
+		GUILayout.Button("Click me");
+		GUILayout.Button("Or me");
+		GUILayout.EndArea();*/
+
+		//Color defaultColor = GUI.color;
+		//GUI.contentColor = new Color(30f / 255f, 167f / 255, 225f / 255f);
+		//GUI.color
+		if (GUILayout.Button("mybutton", SampleStyle.Button, GUILayout.Height(25))) {
+		
+		}
+		if (GUILayout.Button("BUTTON", SampleStyle.ButtonLeft, GUILayout.Height(25))) {
+
+		}
+
+		GUILayout.BeginArea(new Rect(0, 250,Screen.width,50));
+		EditorGUI.DrawRect(new Rect(0, 0, Screen.width, 10), SampleStyle.ColorDarkestBlue);
+		EditorGUI.DrawRect(new Rect(0, 10, Screen.width, 10), SampleStyle.ColorDarkBlue);
+		EditorGUI.DrawRect(new Rect(0, 20, Screen.width, 10), SampleStyle.ColorBlue);
+		EditorGUI.DrawRect(new Rect(0, 30, Screen.width, 10), SampleStyle.ColorLightBlue);
+		EditorGUI.DrawRect(new Rect(0, 40, Screen.width, 10), SampleStyle.ColorLightestBlue);
+		GUILayout.Label("Blue", EditorStyle.HeaderOne); 
+		GUILayout.EndArea();
+		GUILayout.BeginArea(new Rect(0, 300,Screen.width,50));
+		EditorGUI.DrawRect(new Rect(0, 0, Screen.width, 10), SampleStyle.ColorDarkestGreen);
+		EditorGUI.DrawRect(new Rect(0, 10, Screen.width, 10), SampleStyle.ColorDarkGreen);
+		EditorGUI.DrawRect(new Rect(0, 20, Screen.width, 10), SampleStyle.ColorGreen);
+		EditorGUI.DrawRect(new Rect(0, 30, Screen.width, 10), SampleStyle.ColorLightGreen);
+		EditorGUI.DrawRect(new Rect(0, 40, Screen.width, 10), SampleStyle.ColorLightestGreen);
+		GUILayout.Label("Green", EditorStyle.HeaderOne); 
+		GUILayout.EndArea();
+		GUILayout.BeginArea(new Rect(0, 350,Screen.width,50));
+		EditorGUI.DrawRect(new Rect(0, 0, Screen.width, 10), SampleStyle.ColorDarkestGrey);
+		EditorGUI.DrawRect(new Rect(0, 10, Screen.width, 10), SampleStyle.ColorDarkGrey);
+		EditorGUI.DrawRect(new Rect(0, 20, Screen.width, 10), SampleStyle.ColorGrey);
+		EditorGUI.DrawRect(new Rect(0, 30, Screen.width, 10), SampleStyle.ColorLightGrey);
+		EditorGUI.DrawRect(new Rect(0, 40, Screen.width, 10), SampleStyle.ColorLightestGrey);
+		GUILayout.Label("Grey", EditorStyle.HeaderOne); 
+		GUILayout.EndArea();
+		GUILayout.BeginArea(new Rect(0, 400,Screen.width,50));
+		EditorGUI.DrawRect(new Rect(0, 0, Screen.width, 10), SampleStyle.ColorDarkestOrange);
+		EditorGUI.DrawRect(new Rect(0, 10, Screen.width, 10), SampleStyle.ColorDarkOrange);
+		EditorGUI.DrawRect(new Rect(0, 20, Screen.width, 10), SampleStyle.ColorOrange);
+		EditorGUI.DrawRect(new Rect(0, 30, Screen.width, 10), SampleStyle.ColorLightOrange);
+		EditorGUI.DrawRect(new Rect(0, 40, Screen.width, 10), SampleStyle.ColorLightestOrange);
+		GUILayout.Label("Orange", EditorStyle.HeaderOne); 
+		GUILayout.EndArea();
+		GUILayout.BeginArea(new Rect(0, 450,Screen.width,50));
+		EditorGUI.DrawRect(new Rect(0, 0, Screen.width, 10), SampleStyle.ColorDarkestYellow);
+		EditorGUI.DrawRect(new Rect(0, 10, Screen.width, 10), SampleStyle.ColorDarkYellow);
+		EditorGUI.DrawRect(new Rect(0, 20, Screen.width, 10), SampleStyle.ColorYellow);
+		EditorGUI.DrawRect(new Rect(0, 30, Screen.width, 10), SampleStyle.ColorLightYellow);
+		EditorGUI.DrawRect(new Rect(0, 40, Screen.width, 10), SampleStyle.ColorLightestYellow);
+		GUILayout.Label("Yellow", EditorStyle.HeaderOne); 
+		GUILayout.EndArea();
+		//GUI.color = defaultColor;
+		for (int i = 0; i < 5;i++) {
+			GUILayout.Button("mybutton", SampleStyle.Button, GUILayout.Height(100), GUILayout.Width(50));	
+		}
+
+		GUILayout.EndScrollView();
+		Drawing.DrawCircle (new Vector2 (200f, 200f), 100, Color.black, 5,true, 36, 1.0f);
+		Drawing.DrawLine(Vector2.zero, new Vector2(400,400), Color.red, 5, true, 1.0f);
+		Drawing.DrawBezierLine (Vector2.zero, new Vector2 (400, 150), new Vector2 (600, 800), new Vector2 (200, 500), Color.blue, 10, true, 24, 2.0f);
+		//Drawing.DrawCircle(new Vector2(Screen.width / 2, Screen.height / 2), Screen.width / 4, SampleStyle.ColorLightOrange, 5, false, 21, 1.0f);
+		//SampleDrawing.DrawCircle(new Vector2(Screen.width / 2, Screen.height / 2), Screen.width / 4, SampleStyle.ColorLightOrange, 5, true, 21, 1.0f);
 	}
 	void OnInspectorUpdate(){
 		Repaint();	
