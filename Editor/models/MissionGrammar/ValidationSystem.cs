386a24d4d15fb160938b1d71cf6ab530c205db1e
Ze-Hao Wang <salmon.zh.tw@gmail.com>
2017-04-25 10:30:52 +0800

    Fixed No 6.

--- Editor/models/MissionGrammar/ValidationSystem.cs
+++ Editor/models/MissionGrammar/ValidationSystem.cs
@@ -55,12 +55,12 @@ namespace MissionGrammarSystem {
 		// No 1. LeftMoreThanRight.
 		private static bool ValidateLeftMoreThanRight(MissionRule rule, GraphGrammar graphGrammar) {
 
-			return false;
+			return true;
 		}
 		// No 2. EmptyLeft.
 		private static bool ValidateEmptyLeft(MissionRule rule, GraphGrammar graphGrammar) {
 
-			return false;
+			return true;
 		}
 		// No 3. HeadHasParent.
 		private static bool ValidateHeadHasParent(MissionRule rule, GraphGrammar graphGrammar) {
@@ -88,49 +88,36 @@ namespace MissionGrammarSystem {
 		}
 		// No 6. ExactlyDuplicated.
 		private static bool ValidateExactlyDuplicated(MissionRule rule, GraphGrammar graphGrammar) {
-			bool validate = false;
-			// Check the number of connections & nodes first
-			if (rule.SourceRule.Nodes.Count == rule.ReplacementRule.Nodes.Count &&
-			    rule.SourceRule.Connections.Count == rule.ReplacementRule.Connections.Count) {
-				// Check the Connections; alphabetID, connection type, connection start and end
-				for (int i = 0; i < rule.SourceRule.Connections.Count; i++) {
-					if (!(rule.SourceRule.Connections[i].AlphabetID == rule.ReplacementRule.Connections[i].AlphabetID &&
-					    rule.SourceRule.Connections[i].Type == rule.ReplacementRule.Connections[i].Type &&
-					    rule.SourceRule.Connections[i].StartpointStickyOn.AlphabetID == rule.ReplacementRule.Connections[i].StartpointStickyOn.AlphabetID &&
-					    rule.SourceRule.Connections[i].EndpointStickyOn.AlphabetID == rule.ReplacementRule.Connections[i].EndpointStickyOn.AlphabetID)) {
-						validate = true;
-					} 
-				}
-				// Check the Nodes; alphabetID, ordering, and number of children, number of parents, number of sticked connections (type ?)
-				for (int i = 0; i < rule.SourceRule.Nodes.Count; i++) {
-					if (rule.SourceRule.Nodes[i].AlphabetID == rule.ReplacementRule.Nodes[i].AlphabetID &&
-					    rule.SourceRule.Nodes[i].Ordering == rule.ReplacementRule.Nodes[i].Ordering &&
-					    rule.SourceRule.Nodes[i].Children.Count == rule.ReplacementRule.Nodes[i].Children.Count &&
-					    rule.SourceRule.Nodes[i].Parents.Count == rule.ReplacementRule.Nodes[i].Parents.Count &&
-					    rule.SourceRule.Nodes[i].StickiedConnectionsGuid.Count() == rule.ReplacementRule.Nodes[i].StickiedConnectionsGuid.Count()) {
-						// Check the parents; if not equal then it's validated
-						for (int j = 0; j < rule.SourceRule.Nodes[i].Parents.Count; j++) {
-							if (rule.SourceRule.Nodes[i].Parents[j].AlphabetID != rule.ReplacementRule.Nodes[i].Parents[j].AlphabetID ||
-							    rule.SourceRule.Nodes[i].Parents[j].Ordering != rule.ReplacementRule.Nodes[i].Parents[j].Ordering) {
-								validate = true;
-							}
-						}
-						// Check the childrens; if not equal then it's validated
-						for (int j = 0; j < rule.SourceRule.Nodes[i].Children.Count; j++) {
-							if (rule.SourceRule.Nodes[i].Children[j].AlphabetID != rule.ReplacementRule.Nodes[i].Children[j].AlphabetID ||
-							    rule.SourceRule.Nodes[i].Children[j].Ordering != rule.ReplacementRule.Nodes[i].Children[j].Ordering) {
-								validate = true;
-							}
-						}
-					} else {
-						validate = true;
-					}
-				}
-			} else {
-				validate = true;
+			// Check the number of connections & nodes first.
+			if (rule.SourceRule.Nodes.Count       != rule.ReplacementRule.Nodes.Count ||
+			    rule.SourceRule.Connections.Count != rule.ReplacementRule.Connections.Count) {
+				return true;
 			}
-			// Debug.Log("Is Validated? " + validate + ". Duplicated ? " + !validate);
-			return validate;
+			// If find any difference in connections, then defined they are not isomorphic.
+			foreach (var connectionA in rule.SourceRule.Connections) {
+				if (! rule.ReplacementRule.Connections.Exists(connectionB => 
+					connectionB.AlphabetID == connectionA.AlphabetID &&
+					connectionB.Type       == connectionA.Type &&
+					// Check the ordering they sticky on. If null, expresses zero.
+					(connectionB.StartpointStickyOn == null ? 0 : connectionB.StartpointStickyOn.Ordering) == (connectionA.StartpointStickyOn == null ? 0 : connectionA.StartpointStickyOn.Ordering) &&
+					(connectionB.EndpointStickyOn   == null ? 0 : connectionB.EndpointStickyOn.Ordering)   == (connectionA.EndpointStickyOn   == null ? 0 : connectionA.EndpointStickyOn.Ordering)
+				)) {
+					return true;
+				}
+			}
+			// If find any difference in nodes, then defined they are not isomorphic.
+			foreach (var nodeA in rule.SourceRule.Nodes) {
+				if (! rule.ReplacementRule.Nodes.Exists(nodeB =>
+					nodeB.AlphabetID     == nodeA.AlphabetID &&
+					nodeB.Ordering       == nodeA.Ordering &&
+					nodeB.Children.Count == nodeA.Children.Count &&
+					nodeB.Parents.Count  == nodeA.Parents.Count
+				)) {
+					return true;
+				}
+			}
+			// It's illegal and isomorphic.
+			return false;
 		}
 		// No 7. MultipleRelations.
 		private static bool ValidateMultipleRelations(MissionRule rule, GraphGrammar graphGrammar) {
