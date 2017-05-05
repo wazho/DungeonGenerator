584de10654f7124b4e56e18070939da5f71b5c7f
qwe321qwe321qwe321 <aegis321123@gmail.com>
2017-05-05 12:14:52 +0800

    Fix the Valid No.3 bug cuz the arrow modify.

--- Editor/models/MissionGrammar/ValidationSystem.cs
+++ Editor/models/MissionGrammar/ValidationSystem.cs
@@ -68,7 +68,7 @@ namespace MissionGrammarSystem {
 				}
 				foreach (GraphGrammarNode node in graphGrammar.Nodes) {
 					// Connection.StickOn will remain the last node it sticked on, so use position to inforce validation.
-					if (! graphGrammar.Connections.Where(e => (e.StartPosition == node.Position || e.EndPosition == node.Position)).Any()) {
+					if (! graphGrammar.Connections.Where(e => (e.StartpointStickyOn == node || e.EndpointStickyOn == node)).Any()) {
 						return false;
 					}
 				}
