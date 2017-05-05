552db2ed02861cffea78221c6ee022b472184d51
Ze-Hao Wang <salmon.zh.tw@gmail.com>
2017-05-05 16:09:34 +0800

    Checked.

--- Editor/models/MissionGrammar/ValidationSystem.cs
+++ Editor/models/MissionGrammar/ValidationSystem.cs
@@ -68,7 +68,7 @@ namespace MissionGrammarSystem {
 				}
 				foreach (GraphGrammarNode node in graphGrammar.Nodes) {
 					// Connection.StickOn will remain the last node it sticked on, so use position to inforce validation.
-					if (! graphGrammar.Connections.Where(e => (e.StartpointStickyOn == node || e.EndpointStickyOn == node)).Any()) {
+					if (! graphGrammar.Connections.Where(c => (c.StartpointStickyOn == node || c.EndpointStickyOn == node)).Any()) {
 						return false;
 					}
 				}
