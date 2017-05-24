ff5b991c77a875f29242b5d28f9a9f105777314e
harrisonyei <ydharrison0720@gmail.com>
2017-05-24 19:05:01 +0800

    testCommit

--- Editor/models/MissionGrammar/ValidationSystem.cs
+++ Editor/models/MissionGrammar/ValidationSystem.cs
@@ -6,7 +6,7 @@ using System.Collections;
 using System.Collections.Generic;
 // Locales.
 using Languages = LanguageManager;
-// TESTCommit26
+// TESTCommit27
 namespace MissionGrammarSystem {
 	// Label of the validation system in mission rule.
 	public enum ValidationLabel {
